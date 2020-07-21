386ee1a6c4daab0dcf94d363399484758ef96d6d
Sanghelyos <megadarkspartan@laposte.net>
2020-07-21 14:50:03 +0200

    Ajout de config

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
     /*/////////////////////////////
     * Affichage des erreurs
     *//////////////////////////////
-ini_set('display_startup_errors', true);
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-
+// ini_set('display_startup_errors', true);
+// error_reporting(E_ALL);
+// ini_set('display_errors', true);
+define('DIR_NAME','SangliMVC');
 
     /*/////////////////////////////
     * Dépendances
     *//////////////////////////////
+require_once('assets/Models/Config.php');
 require_once('assets/Models/Autoload.php');
 
     /*/////////////////////////////
